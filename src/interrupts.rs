4e637aa78ba94afd798b7e61bc17c58fea250f39
zhangjun <827622690@qq.com>
2024-07-22 00:34:42 +0800

    内存分页初探

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,10 +1,11 @@
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode};
 use crate::println;
 use lazy_static::lazy_static;
 use crate::gdt;
 use pic8259::ChainedPics;
 use spin;
 use crate::print;
+use crate::hlt_loop;
 
 // static mut IDT: InterruptDescriptorTable = InterruptDescriptorTable::new();
 
@@ -43,10 +44,15 @@ lazy_static! {
             idt.double_fault.set_handler_fn(double_fault_handler)
                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
         }
+
         idt[InterruptIndex::Timer.as_usize()]
             .set_handler_fn(timer_interrupt_handler);
+
         idt[InterruptIndex::Keyboard.as_usize()]
         .set_handler_fn(keyboard_interrupt_handler);
+
+        idt.page_fault.set_handler_fn(page_fault_handler);
+
         idt
     };
 }
@@ -153,4 +159,17 @@ extern "x86-interrupt" fn keyboard_interrupt_handler(
 
 
 
+}
+
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: PageFaultErrorCode,
+) {
+    use x86_64::registers::control::Cr2;
+
+    println!("EXCEPTION: PAGE FAULT");
+    println!("Accessed Address: {:?}", Cr2::read());
+    println!("Error Code: {:?}", error_code);
+    println!("{:#?}", stack_frame);
+    hlt_loop();
 }
--- src/main.rs
+++ src/main.rs
@@ -22,9 +22,23 @@ pub extern "C" fn _start() -> ! {
     // fn stack_overflow() {
     //     stack_overflow(); // 每一次递归都会将返回地址入栈
     // }
-
     // // 触发 stack overflow
     // stack_overflow();
+
+    // let ptr = 0xdeadbeaf as *mut u8;
+    // unsafe { *ptr = 42; }
+
+    // let ptr = 0x2031b2 as *mut u8;
+    // // read from a code page
+    // unsafe { let x = *ptr; }
+    // println!("read worked");
+    // // write to a code page
+    // unsafe { *ptr = 42; }
+    // println!("write worked");
+
+    use x86_64::registers::control::Cr3;
+    let (level_4_page_table, _) = Cr3::read();
+    println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
 
     #[cfg(test)]
     test_main();
